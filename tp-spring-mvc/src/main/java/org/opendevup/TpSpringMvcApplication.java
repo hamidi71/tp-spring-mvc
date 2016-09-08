74fba814e6e3c8af3d63ad9b54bf50b1dfb4b7ac
hamidi71 <chiahna71@gmail.com>
2016-09-08 14:51:42 +0200

    Github test

--- tp-spring-mvc/src/main/java/org/opendevup/TpSpringMvcApplication.java
+++ tp-spring-mvc/src/main/java/org/opendevup/TpSpringMvcApplication.java
@@ -13,7 +13,7 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 
-
+//github testen
 @SpringBootApplication
 public class TpSpringMvcApplication {
 
